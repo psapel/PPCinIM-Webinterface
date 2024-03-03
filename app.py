c006766c96322813500fadd8f5fc92c27944270b
annga <75178572+Anna-grf@users.noreply.github.com>
2024-03-03 14:18:17 +0100

    refind starts with search assets

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, url_for, jsonify
 from flask_cors import CORS
+import json
+import os
+
+from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 CORS(app)
@@ -54,19 +58,166 @@ def get_assets():
 def search_assets():
     try:
         search_query = request.args.get('q')
-        
+
         if search_query:
             # Filter assets based on the search query
-            filtered_assets = [asset for asset in memory_storage if 
-                               search_query.lower() in asset['assetName'].lower() or
-                               search_query.lower() in asset['assetType'].lower() or
-                               any(search_query.lower() in category.lower() for category in asset['assetCategories'])]
+            filtered_assets = [asset for asset in memory_storage if
+                               any(word.lower().startswith(search_query.lower()) for word in asset['assetName'].split()) or
+                               any(word.lower().startswith(search_query.lower()) for word in asset['assetType'].split()) or
+                               any(category.lower().startswith(search_query.lower()) for category in asset['assetCategories'])]
             return jsonify(filtered_assets), 200
         else:
             return jsonify(memory_storage), 200
     except Exception as e:
         print(e)
         return jsonify({"error": "Failed to retrieve assets"}), 500
+
+
+es = Elasticsearch(hosts=['http://localhost:9200'])
+
+index_settings = {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0,
+        "refresh_interval": "5s"
+    },
+    "mappings": {
+        "properties": {
+            "GrahamNotation": {
+                "properties": {
+                    "https://www.iop.rwth-aachen.de/PPC/1/1/machineEnvironment": {"type": "keyword"},
+                    "https://www.iop.rwth-aachen.de/PPC/1/1/schedulingConstraints": {"type": "keyword"},
+                    "https://www.iop.rwth-aachen.de/PPC/1/1/schedulingObjectiveFunction": {"type": "keyword"}
+                }
+            }
+        }
+    }
+}
+
+index_name = 'new_search'
+
+if not es.indices.exists(index=index_name):
+    # es.indices.create(index=index_name, body={"settings": index_settings["settings"]})
+    es.indices.create(index=index_name, body={"settings": index_settings["settings"]},
+                      mappings=index_settings["mappings"])
+
+def load_models(es):
+    model_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src/pages/decisionsupport/jsonModels'))
+    models = []
+    for filename in os.listdir(model_folder):
+        if filename.endswith('.json'):
+            model_path = os.path.join(model_folder, filename)
+            try:
+                with open(model_path, 'r', encoding='utf-8') as f:
+                    model_data = json.load(f)
+                    model_id = model_data.get('_id')
+                    if model_id:
+                        print(f"Original Model ID: {model_id}")
+                        es.index(index=index_name, id=model_id, document=model_data["GrahamNotation"])
+                        print(f"Model data: {model_data}")
+                        print(f"Indexed model with ID: {model_id}")
+                        models.append(model_data)
+                    else:
+                        print(f"Model ID not found in JSON: {model_path}")
+            except FileNotFoundError:
+                print(f"Failed to load model: {model_path}")
+
+    return models
+
+
+# Call the load_models function to start loading the JSON files
+loaded_models = load_models(es)
+
+def find_matching_model(es, url1, url2, url3):
+    len_2 = len(url2)
+    len_3 = len(url3)
+
+    query = {
+        "query": {
+            "bool": {
+                "must":
+                    [{"match_phrase": {"https://www.iop.rwth-aachen.de/PPC/1/1/machineEnvironment": url1}},
+                     {"terms": {
+                         "https://www.iop.rwth-aachen.de/PPC/1/1/schedulingConstraints.keyword": url2}},
+                     {
+                         "script":
+                             {
+                                 "script":
+                                     {
+                                         "source": "doc['https://www.iop.rwth-aachen.de/PPC/1/1/schedulingConstraints.keyword'].length == params.fixed_array_length",
+                                         "params": {
+                                             "fixed_array_length": len_2
+                                         }
+                                     }
+                             }
+                     },
+                     {
+                         "terms": {
+                             "https://www.iop.rwth-aachen.de/PPC/1/1/schedulingObjectiveFunction.keyword": url3}},
+
+                     {
+                         "script":
+                             {
+                                 "script":
+                                     {
+                                         "source": "doc['https://www.iop.rwth-aachen.de/PPC/1/1/schedulingObjectiveFunction.keyword'].length == params.fixed_array_length",
+                                         "params": {
+                                             "fixed_array_length": len_3
+                                         }
+                                     }
+                             }
+                     }
+
+                     ]
+
+            }
+        }
+
+    }
+
+    print("Elasticsearch Query:", query)
+
+    result = es.search(index='new_search', size=16, body=query)
+    hits = result.get('hits', {}).get('hits', [])
+
+    print("Number of hits:", len(hits))
+
+    if hits:
+        for hit in hits:
+            source = hit.get('_source')
+            print("Retrieved Document:", source)
+
+    return hits
+
+# Route to handle the search functionality
+@app.route('/api/search', methods=['POST'])
+def search():
+    data = request.get_json()
+
+    machine_environment = data.get('machine_environment')
+    scheduling_constraints = data.get('scheduling_constraints')
+    scheduling_objective_function = data.get('scheduling_objective_function')
+
+    matching_model = find_matching_model(es, machine_environment, scheduling_constraints,
+                                         scheduling_objective_function)
+
+    selected_models = []
+    for hit in matching_model:
+        source = hit.get('_source', {})
+        selected_models.append(source)
+
+    return jsonify(selected_models)
+
+# Route to handle the main page
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Handle form submission or redirect as needed
+        # You may want to redirect to another page or render a template
+        pass
+    else:
+        # Render the main page or template
+        pass
 
 
 if __name__ == '__main__':
--- src/pages/decisionsupport/Decisionsupport.tsx
+++ src/pages/decisionsupport/Decisionsupport.tsx
@@ -1,82 +1,45 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Decisionsupport.css";
-import jsonDocument_0 from "./jsonModels/jsonDocument_0.json";
-import jsonDocument_1 from "./jsonModels/jsonDocument_1.json";
-import jsonDocument_2 from "./jsonModels/jsonDocument_2.json";
-import jsonDocument_3 from "./jsonModels/jsonDocument_3.json";
-import jsonDocument_4 from "./jsonModels/jsonDocument_4.json";
-import jsonDocument_5 from "./jsonModels/jsonDocument_5.json";
-import jsonDocument_6 from "./jsonModels/jsonDocument_6.json";
-import jsonDocument_7 from "./jsonModels/jsonDocument_7.json";
-import jsonDocument_8 from "./jsonModels/jsonDocument_8.json";
-import jsonDocument_9 from "./jsonModels/jsonDocument_9.json";
-import jsonDocument_10 from "./jsonModels/jsonDocument_10.json";
-import jsonDocument_11 from "./jsonModels/jsonDocument_11.json";
-import jsonDocument_12 from "./jsonModels/jsonDocument_12.json";
-import jsonDocument_13 from "./jsonModels/jsonDocument_13.json";
-import jsonDocument_14 from "./jsonModels/jsonDocument_14.json";
-import jsonDocument_15 from "./jsonModels/jsonDocument_15.json";
-import jsonDocument_16 from "./jsonModels/jsonDocument_16.json";
-
-const models = [
-  jsonDocument_0,
-  jsonDocument_1,
-  jsonDocument_2,
-  jsonDocument_3,
-  jsonDocument_4,
-  jsonDocument_5,
-  jsonDocument_6,
-  jsonDocument_7,
-  jsonDocument_8,
-  jsonDocument_9,
-  jsonDocument_10,
-  jsonDocument_11,
-  jsonDocument_12,
-  jsonDocument_13,
-  jsonDocument_14,
-  jsonDocument_15,
-  jsonDocument_16,
-];
 
 const categories = [
   {
     title: "α - Machine Environments",
     criteria: [
       {
-        name: "Single Machine",
+        name: "Single machine (1)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/singleMachine",
       },
       {
-        name: "Identical machines in parallel",
+        name: "Identical machines in parallel (Pm)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/parallelMachines",
       },
       {
-        name: "Machines in parallel with different speeds",
-        url: "https://www.iop.rwth-aachen.de/PPC/1/1/",
+        name: "Machines in parallel with different speeds (Qm)",
+        url: "https://www.iop.rwth-aachen.de/PPC/1/1/uniformMachines",
       },
       {
-        name: "Unrelated machines in parallel",
+        name: "Unrelated machines in parallel (Rm)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/unrelatedMachines",
       },
       {
-        name: "Flow shop",
+        name: "Flow shop (Fm)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/flowShop",
       },
       {
-        name: "Flexible Flow shop",
+        name: "Flexible flow shop (FFc)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/flexibleFlowShop",
       },
       {
-        name: "Job shop",
+        name: "Job shop (Jm)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/jobShop",
       },
       {
-        name: "Flexible Job shop",
+        name: "Flexible job shop (FJc)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/flexibleJobShop",
       },
       {
-        name: "Open shop",
+        name: "Open shop (Om)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/openShop",
       },
     ],
@@ -86,53 +49,59 @@ const categories = [
     title: "β - Scheduling Constraints",
     criteria: [
       {
-        name: "Release Dates",
+        name: "Release dates (rj)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/releaseDates",
       },
       {
-        name: "Preemptions",
+        name: "Preemptions (prmp)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/preemptions",
       },
       {
-        name: "Precedence Constraints",
-        url: "https://www.iop.rwth-aachen.de/PPC/1/1/precedenceConstraints",
+        name: "Precedence constraints (prec)",
+        url: "https://www.iop.rwth-aachen.de/PPC/1/1/precedence",
       },
       {
-        name: "Sequence dependent setup times",
+        name: "Sequence dependent setup times (si,j)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/seqDepSetup",
       },
       {
-        name: "Job families",
+        name: "Job families (fmls)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/jobFamilies",
       },
       {
-        name: "Batch Processing",
+        name: "Batch processing (batch(b))",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/batch",
       },
       {
-        name: "Breakdown",
+        name: "Breakdown (brkdwn)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/breakdown",
       },
       {
-        name: "Machine eligibilty restrictions",
+        name: "Machine eligibility restrictions (Mj)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/maschEligibility",
       },
       {
-        name: "Permutation",
+        name: "Permutation (prmu)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/permutation",
       },
       {
-        name: "Blocking",
+        name: "Blocking (block)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/blocking",
       },
-      { name: "No-Wait", url: "https://www.iop.rwth-aachen.de/PPC/1/1/noWait" },
       {
-        name: "Recirculation",
+        name: "No-wait (nwt)",
+        url: "https://www.iop.rwth-aachen.de/PPC/1/1/noWait",
+      },
+      {
+        name: "Recirculation (rcrc)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/recirculation",
       },
-      { name: "oprtr", url: "https://www.iop.rwth-aachen.de/PPC/1/1/oprtr" },
       {
-        name: "Tardiness Penalty",
+        name: "oprtr (oprtr)",
+        url: "https://www.iop.rwth-aachen.de/PPC/1/1/oprtr",
+      },
+      {
+        name: "Tardiness Penalty (LCj)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/tardinessPenalty",
       },
     ],
@@ -141,31 +110,31 @@ const categories = [
     title: "γ - Objective Function",
     criteria: [
       {
-        name: "Makespan",
-        url: "https://www.iop.rwth-aachen.de/PPC/1/1/makespan",
-      },
-      {
-        name: "Maximum Lateness",
+        name: "Makespan (Cmax)",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/maxCompletionTime",
       },
       {
-        name: "Total weighted completion time",
+        name: "Maximum Lateness (Lmax)",
+        url: "https://www.iop.rwth-aachen.de/PPC/1/1/maxLateness",
+      },
+      {
+        name: "Total weighted completion time (Σ(wjCj))",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/weightedSumCompletionTime",
       },
       {
-        name: "Total completion time",
-        url: "https://www.iop.rwth-aachen.de/PPC/1/1/maxCompletionTime",
+        name: "Total completion time (ΣCj)",
+        url: "http://www.iop.rwth-aachen.de/PPC/1/1/sumCompletionTime",
       },
       {
-        name: "Total tardiness",
+        name: "Total tardiness (Σ(Tj))",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/sumTardiness",
       },
       {
-        name: "Total weighted tardiness",
+        name: "Total weighted tardiness (Σ(wjTj))",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/weightedSumTardiness",
       },
       {
-        name: "Total earliness for each job",
+        name: "Total earliness for each job (Σ(Ej))",
         url: "https://www.iop.rwth-aachen.de/PPC/1/1/sumEarliness",
       },
     ],
@@ -178,7 +147,6 @@ const Decisionsupport = () => {
   const [selectedEnvironment, setSelectedEnvironment] = useState("");
 
   const handleCheckboxChange = (event) => {
-    console.log(event);
     setSelectedCriteria({
       ...selectedCriteria,
       [event.target.value]: event.target.checked,
@@ -189,26 +157,35 @@ const Decisionsupport = () => {
     setSelectedEnvironment(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
-    const filteredModels = models.filter((model) => {
-      // Check if selected criteria is true
-      return Object.keys(selectedCriteria).some((criterionUrl) => {
-        const grahamNotationValues = Object.values(model.GrahamNotation);
-        const urls = grahamNotationValues
-          .flatMap((value) => (Array.isArray(value) ? value : []))
-          .concat(
-            grahamNotationValues.filter(
-              (value) => typeof value === "string" && value.startsWith("http")
-            )
-          );
-        return urls.includes(criterionUrl);
-      });
-    });
     console.log("selected criteria", selectedCriteria);
     console.log("selected environment", selectedEnvironment);
-    console.log("filtered models", filteredModels);
+  
+    try {
+      const response = await fetch('http://localhost:5000/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          machine_environment: selectedEnvironment,
+          scheduling_constraints: Object.keys(selectedCriteria),
+          scheduling_objective_function: [],
+        }),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Filtered Models from Backend:", data.filteredModels);
+  
+        navigate('/filtered-model', { state: { filteredModels: data.filteredModels } });
+      } else {
+        console.error('Failed to fetch filtered models from the backend');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+    }
   };
 
   return (
