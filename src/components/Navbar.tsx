e41d827cff2ebae858ef9f8d78faba984056ece6
Patrick Sapel <patrick.sapel@gmx.de>
2024-03-14 15:10:26 +0100

    Logo size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,7 +53,7 @@ const Navbar = () => {
           </ul>
         </div>
         <a className="" onClick={() => navigate("/")}>
-          <img src={logo} alt="Logo" className="navbar-logo" />
+          <img src={logo} alt="Logo" className="navbar-logo" width="80%"/>
         </a>
       </div>
       <div className="navbar-center hidden lg:flex">
