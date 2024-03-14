5777737ca23815b4eb83b678f57b11f2c5c4ee93
Patrick Sapel <patrick.sapel@gmx.de>
2024-03-14 15:14:57 +0100

    Change height of logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,7 +53,7 @@ const Navbar = () => {
           </ul>
         </div>
         <a className="" onClick={() => navigate("/")}>
-          <img src={logo} alt="Logo" className="navbar-logo" height="10%"/>
+          <img src={logo} alt="Logo" className="navbar-logo" style={{ height: '80%' }}/>
         </a>
       </div>
       <div className="navbar-center hidden lg:flex">
