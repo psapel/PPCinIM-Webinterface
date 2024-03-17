81bbb209521759f96206bac445e57df794aad16c
priscilliacw <priscilliawangi@gmail.com>
2024-03-17 01:47:31 +0100

    add back button to production planning

--- src/pages/pp/Results.tsx
+++ src/pages/pp/Results.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const queryTypeMapping = {
   query1: "Temperature Control Unit Query",
@@ -10,6 +10,7 @@ const queryTypeMapping = {
 const Results = () => {
   const { queryType } = useParams();
   const [responseData, setResponseData] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,80 +28,103 @@ const Results = () => {
   const queryName = queryTypeMapping[queryType];
 
   return (
-    <div className="overflow-x-auto results">
-      <h1 className="resultsfont">{queryName} Result</h1>
-      {queryName === "Temperature Control Unit Query" && (
-        <table className="table table-zebra">
-          <thead>
-            <tr>
-              <th>InquiryCoolant</th>
-              <th>Coolant Comparison</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.isArray(responseData.data) &&
-              responseData.data.length > 0 &&
-              responseData.data.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.InquiryCoolant}</td>
-                  <td>
-                    {Array.isArray(item.MatchingControlUnits) &&
-                      item.MatchingControlUnits.map((unit, index) => (
-                        <p key={index}>Matched with: {unit.idShort}</p>
-                      ))}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      )}
+    <div>
+      <div className="button">
+        <button
+          className="btn btn-circle btn-outline"
+          onClick={() => navigate("/production-planning")}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="overflow-x-auto results">
+        <h1 className="resultsfont">{queryName} Result</h1>
+        {queryName === "Temperature Control Unit Query" && (
+          <table className="table table-zebra">
+            <thead>
+              <tr>
+                <th>InquiryCoolant</th>
+                <th>Coolant Comparison</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.isArray(responseData.data) &&
+                responseData.data.length > 0 &&
+                responseData.data.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.InquiryCoolant}</td>
+                    <td>
+                      {Array.isArray(item.MatchingControlUnits) &&
+                        item.MatchingControlUnits.map((unit, index) => (
+                          <p key={index}>Matched with: {unit.idShort}</p>
+                        ))}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        )}
 
-      {queryName === "Handling Device Query" && (
-        <table className="table table-zebra">
-          <thead>
-            <tr>
-              <th>InquiryHandlingDevice</th>
-              <th>Handling Device Comparison</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.isArray(responseData.data) &&
-              responseData.data.length > 0 &&
-              responseData.data.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.InquiryHandlingDevice}</td>
+        {queryName === "Handling Device Query" && (
+          <table className="table table-zebra">
+            <thead>
+              <tr>
+                <th>InquiryHandlingDevice</th>
+                <th>Handling Device Comparison</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.isArray(responseData.data) &&
+                responseData.data.length > 0 &&
+                responseData.data.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.InquiryHandlingDevice}</td>
 
-                  <td>
-                    {Array.isArray(item.MatchingControlUnits) &&
-                      item.MatchingControlUnits.map((unit, index) => (
-                        <p key={index}>Matched with: {unit.idShort}</p>
-                      ))}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      )}
+                    <td>
+                      {Array.isArray(item.MatchingControlUnits) &&
+                        item.MatchingControlUnits.map((unit, index) => (
+                          <p key={index}>Matched with: {unit.idShort}</p>
+                        ))}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        )}
 
-      {queryName === "Injection Molding Machine Query" && (
-        <table className="table table-zebra">
-          <thead>
-            <tr>
-              <th>Injection Molding Machine ID</th>
-              <th>Feasibility</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.isArray(responseData.data) &&
-              responseData.data.map((record, index) => (
-                <tr key={index}>
-                  <td>{record.idShort}</td>
-                  <td>{record.feasibility}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      )}
+        {queryName === "Injection Molding Machine Query" && (
+          <table className="table table-zebra">
+            <thead>
+              <tr>
+                <th>Injection Molding Machine ID</th>
+                <th>Feasibility</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.isArray(responseData.data) &&
+                responseData.data.map((record, index) => (
+                  <tr key={index}>
+                    <td>{record.idShort}</td>
+                    <td>{record.feasibility}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 };
